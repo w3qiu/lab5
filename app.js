c0b8825cb3b3abb4ab7da3f38f2485adba6a0a4d
w3qiu <damon.q26@gmail.com>
2020-02-06 13:25:15 -0800

    finish lab5

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var http = require('http');
 var path = require('path');
 var handlebars = require('express3-handlebars')
 
+var add = require('./routes/add');
 var index = require('./routes/index');
 // Example route
 // var user = require('./routes/user');
@@ -35,7 +36,9 @@ if ('development' == app.get('env')) {
 }
 
 // Add routes here
+app.get('/add', add.addFriend);
 app.get('/', index.view);
+
 // Example route
 // app.get('/users', user.list);
 
--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,12 @@
 var data = require("../data.json");
 
-exports.addFriend = function(request, response) {    
-	// Your code goes here
- }
+exports.addFriend = function(request, response)
+{
+    var n = request.query.name;
+    var d = request.query.description;
+    var newFri = { "name": n, "description": d, "imageURL": "http://lorempixel.com/400/400/people"};
+    console.log(newFri);
+    data.friends.push(newFri);
+    response.render('index', data);
+    
+};
